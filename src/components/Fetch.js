88e436c71174727ec614d4df8cff11412ffc7744
Marcus Walby <marcuswalby24@gmail.com>
2022-07-04 21:15:59 -0400

    page renders correct data but only on re render triggerd by change in code

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -18,27 +18,37 @@ export default function Fetch() {
         const res = await fetch(
           `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${KEY}`
         );
-        const data = await res.json();
-        setLat(data[0].lat)
-        setLon(data[0].lon)
-        console.log(data)
+        const setData = await res.json();
+        setLat(setData[0].lat)
+        setLon(setData[0].lon)
       } catch (e) {
         console.error(e);
       }
     };
+    
     fetchCityData();
-    const fetchData = async () => {
-      const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${KEY}`
-      );
-      const data = await res.json();
-      setWeatherData(data);
-      console.log(data);
-      setLoading(false)
-    };
-    fetchData();
   }, []);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${KEY}`
+        );
+        const data = await res.json();
+        setWeatherData(data);
+        console.log(data);
+        setLoading(false)
+      } catch (e) {
+        console.error(e)
+      }
+    };
+    fetchData();
+  },[])
+
+  
+  console.log(lat)
   return (
     <>
       <div className="searchBar">
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ export default function SearchBar(props) {
   return (
     <>
     <form>
-      <input type="text" placeholder='Enter city or town name' onSubmit={event => {console.log(event.target.value)}}/>
+      <input type="text" placeholder='Enter city or town name' onSubmit={event => {event.preventDefault();console.log(event.target.value)}}/>
       <button type='submit'>search</button>
     </form>
     </>
