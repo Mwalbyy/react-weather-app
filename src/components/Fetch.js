ce0df7474348f4b4b372903605ec27a575f3f834
Marcus Walby <marcuswalby24@gmail.com>
2022-07-06 15:42:13 -0400

    search function works, but takes three key presses for some odd reason...HOW FUN IS THIS

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -5,60 +5,70 @@ import WeatherInfo from "./WeatherInfo";
 const KEY = "c9648bd67b02cb074a8cff7f4418e8ec";
 const part = "hourly";
 
-
-
 export default function Fetch() {
-
-  const handleSubmit = (thing) => {
-
-   
-
-   setCity(thing)
-  };
-
   const [weatherData, setWeatherData] = useState([]);
   const [lat, setLat] = useState(0);
   const [lon, setLon] = useState(0);
   const [city, setCity] = useState("leonia");
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchCityData = async () => {
-      try {
-        let res = await fetch(
-          `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${KEY}`
-        );
-        const setData = await res.json()
-        const cityDataChange = ((setData) => {
-          setLat(setData[0].lat)
-          setLon(setData[0].lon)
-        })
-        cityDataChange()
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    
-    fetchCityData();
-  }, []);
+  const handleSubmit = async (event) => {
+    if(event.key === 'Enter'){
+      event.preventDefault()
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
+    setCity(event.target.value);
+    let res = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${KEY}`
+    );
+    const setData = await res.json();
+    setLat(setData[0].lat);
+    setLon(setData[0].lon);
+    let mainRes = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${KEY}`
+    );
+    const data = await mainRes.json();
+    setWeatherData(data);
+    console.log(data)
+    setLoading(false);}
+  };
 
-        let res = await fetch(
-          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${KEY}`
-        );
-        const data = await res.json();
-        setWeatherData(data);
-        console.log(data);
-        setLoading(false)
-      } catch (e) {
-        console.error(e)
-      }
-    };
-    fetchData();
-  },[])
+  // useEffect(() => {
+  //   const fetchCityData = async () => {
+  //     try {
+  //       let res = await fetch(
+  //         `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${KEY}`
+  //       );
+  //       const setData = await res.json()
+  //       const cityDataChange = ((setData) => {
+  //         setLat(setData[0].lat)
+  //         setLon(setData[0].lon)
+  //       })
+  //       cityDataChange()
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+  //   };
+
+  //   fetchCityData();
+  // }, []);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+
+  //       let res = await fetch(
+  //         `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${KEY}`
+  //       );
+  //       const data = await res.json();
+  //       setWeatherData(data);
+  //       console.log(data);
+  //       setLoading(false)
+  //     } catch (e) {
+  //       console.error(e)
+  //     }
+  //   };
+  //   fetchData();
+  // },[])
   return (
     <>
       <div className="searchBar">
@@ -67,14 +77,28 @@ export default function Fetch() {
 
       <div className="weatherData">
         <WeatherInfo />
-        <h1>{loading ? 'hello' : JSON.stringify(weatherData.daily[0].temp.day)}</h1>
-        
+        <h1>
+          {loading ? "hello" : JSON.stringify(weatherData.daily[0].temp.day)}
+        </h1>
       </div>
       <div>
-      <form>
-      <input type="text" placeholder='Enter city or town name' onChange={event => {event.preventDefault();handleSubmit(event.target.value)}} value={city}/>
-      <button type='submit' onSubmit={event => {event.preventDefault()}}>search</button>
-    </form>
+        <form>
+          <input
+            type="text"
+            placeholder="Enter city or town name"
+            onKeyDown={(event) => {
+              handleSubmit(event);
+            }}
+          />
+          {/* <button
+            type="submit"
+            onSubmit={(event) => {
+              event.preventDefault();
+            }}
+          >
+            search
+          </button> */}
+        </form>
       </div>
     </>
   );
