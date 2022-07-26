69c8cd0d88c6f36672336177e55c06843865a3fc
Marcus Walby <marcuswalby24@gmail.com>
2022-07-26 18:59:32 -0400

    formating issue

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -3,43 +3,41 @@ import { ParallaxLayer } from "@react-spring/parallax";
 export default function WeatherInfo(props) {
   return (
     <>
-      
-        <ParallaxLayer offset={0} speed={0.5} factor={2}>
-          <ul className="listOfStates">
-            {props.loading
-              ? "loading"
-              : props.citySelection?.map((city) => {
-                  return (
-                    <form>
-                      <li>
-                        <h1>{city.name}</h1>
-                        <h1>{city.state}</h1>
-                        <button
-                          type="submit"
-                          onClick={(event) => {
-                            props.setLoadingWeather(true);
-                            props.setLatLon([city.lat, city.lon]);
-                            props.chooseCity(event);
-                            props.handleSubmit(event);
-                            props.setLoadingWeather(false);
-                          }}
-                        >
-                          this state
-                        </button>
-                      </li>
-                    </form>
-                  );
-                })}
-          </ul>
-        </ParallaxLayer>
-        <ParallaxLayer>
-
-      <h1 className="weatherInfo">
-        {props.loadingWeather
-          ? "Loading"
-          : JSON.stringify(props.weatherData?.daily[0]?.temp?.day)}
-      </h1>
-        </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={0.5} factor={2}>
+        <ul className="listOfStates">
+          {props.loading
+            ? "loading"
+            : props.citySelection?.map((city) => {
+                return (
+                  <form>
+                    <li>
+                      <h1>{city.name}</h1>
+                      <h1>{city.state}</h1>
+                      <button
+                        type="submit"
+                        onClick={(event) => {
+                          props.setLoadingWeather(true);
+                          props.setLatLon([city.lat, city.lon]);
+                          props.chooseCity(event);
+                          props.handleSubmit(event);
+                          props.setLoadingWeather(false);
+                        }}
+                      >
+                        this state
+                      </button>
+                    </li>
+                  </form>
+                );
+              })}
+        </ul>
+      </ParallaxLayer>
+      <ParallaxLayer>
+        <h1 className="weatherInfo">
+          {props.loadingWeather
+            ? "Loading"
+            : JSON.stringify(props.weatherData?.daily[0]?.temp?.day)}
+        </h1>
+      </ParallaxLayer>
     </>
   );
 }
