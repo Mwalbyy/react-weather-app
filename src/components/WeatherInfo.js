89cb16a8e7f9244f74c99152abf0c54955452264
Marcus Walby <marcuswalby24@gmail.com>
2022-07-26 18:51:26 -0400

    added base of front end parralax

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,32 +1,38 @@
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
 export default function WeatherInfo(props) {
   return (
     <>
-    <ul>
-          {props.loading
-            ? "loading"
-            : props.citySelection?.map((city) => {
-                return (
-                  <form>
-                    <li>
-                      <h1>{city.name}</h1>
-                      <h1>{city.state}</h1>
-                      <button
-                        type="submit"
-                        onClick={(event) => {
-                          props.setLoadingWeather(true);
-                          props.setLatLon([city.lat, city.lon]);
-                          props.chooseCity(event);
-                          props.handleSubmit(event)
-                          props.setLoadingWeather(false);
-                        }}
-                      >
-                        this state
-                      </button>
-                    </li>
-                  </form>
-                );
-              })}
-        </ul>
+      <Parallax pages={3}>
+        <ParallaxLayer offset={0} speed={0.5} factor={2}>
+          <ul className="listOfStates">
+            {props.loading
+              ? "loading"
+              : props.citySelection?.map((city) => {
+                  return (
+                    <form>
+                      <li>
+                        <h1>{city.name}</h1>
+                        <h1>{city.state}</h1>
+                        <button
+                          type="submit"
+                          onClick={(event) => {
+                            props.setLoadingWeather(true);
+                            props.setLatLon([city.lat, city.lon]);
+                            props.chooseCity(event);
+                            props.handleSubmit(event);
+                            props.setLoadingWeather(false);
+                          }}
+                        >
+                          this state
+                        </button>
+                      </li>
+                    </form>
+                  );
+                })}
+          </ul>
+        </ParallaxLayer>
+      </Parallax>
       <h1>
         {props.loadingWeather
           ? "Loading"
